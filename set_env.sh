587a0b83a8e261c37e495d478809329d84b87458
Adrian Mitchell <dbledime@yahoo.com>
2021-08-09 16:29:39 -0500

    inital deploy

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgresdb
+export POSTGRES_PASSWORD=denier93
+export POSTGRES_HOST=db-adm-p3.c4asaqqbaqtj.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::adm-p3
+export AWS_REGION=us-east-2
+export AWS_PROFILE=adrian
+export JWT_SECRET=hello
+export URL=http://localhost:8100
