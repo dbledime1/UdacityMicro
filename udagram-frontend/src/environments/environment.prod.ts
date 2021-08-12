2d52812ed959b748c5fed16e0620ca0d25172ea5
Adrian Mitchell <dbledime@yahoo.com>
2021-08-12 08:39:22 -0500

    removal os set.env info

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac604a4fd04c6486a89697fb36ba7707-1800843334.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http:// adbcfca388b55463183760b1d4dcbaf3-1105217095.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
